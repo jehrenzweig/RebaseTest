8ba379870fa0b9b777c715e1c92c99e0deb6afc1
Jesse Ehrenzweig <jesse@supersaiyan.com>
2018-04-19 17:02:56 -0400

    Create variable "b3"; fix merge error

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,9 +1,11 @@
 ﻿namespace ClassLibrary1
 {
-    public class Class1
-    {
+	public class Class1
+	{
 		private int a = 1;
 		private int a2 = 2;
 		private bool b = true;
 		private bool b2 = false;
+		private bool b3 = false;
+	}
 }
